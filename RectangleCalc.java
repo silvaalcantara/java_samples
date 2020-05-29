e6876ac8870847dc27cec9aa7f8597ff81f3860d
silvaalcantara <allan.pa.br@gmail.com>
2020-05-29 11:37:43 -0300

    Calculation samples

---
+++ RectangleCalc.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rectangle;
+
+public class RectangleCalc {
+    
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Enter rectangle width and height: ");
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+        System.out.printf("AREA: %.02f \n", rectangle.area());
+        System.out.printf("PERIMETER: %.02f\n", rectangle.perimeter());
+        System.out.printf("DIAGONAL: %.02f\n", rectangle.diagonal());
+
+        sc.close();
+    }
+
+}
