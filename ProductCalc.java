f55ffefc9d52283093b4e6791c19e6c18388dd7b
silvaalcantara <allan.pa.br@gmail.com>
2020-05-31 21:29:11 -0300

    Samples with encapsulation examples

--- ProductCalc.java
+++ ProductCalc.java
@@ -1,6 +1,5 @@
 import java.util.Locale;
 import java.util.Scanner;
-
 import entities.Product;
 
 public class ProductCalc {
@@ -10,21 +9,22 @@ public class ProductCalc {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
         System.out.print("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
 
         System.out.println();
         System.out.println("Product data: " + product);
 
         System.out.println();
         System.out.print("Enter the number of products to be added in stock: ");
-        int quantity = sc.nextInt();
+        quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
