c6be8ad1c26e489fb915ee70d68a377a5c00fb48
silvaalcantara <allan.pa.br@gmail.com>
2020-06-04 20:54:06 -0300

    list samples

--- entities/Product.java
+++ entities/Product.java
@@ -2,14 +2,23 @@ package entities;
 
 public class Product {
     
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
     public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return this.price;
     }
 
     public double totalValueInStock() {
--- entities/Student.java
+++ entities/Student.java
@@ -3,10 +3,26 @@ package entities;
 public class Student {
 
     public String name;
+    public String email;
     public double firstGrade;
     public double secondGrade;
     public double thirdGrade;
+
+    public Student() {}
+
+    public Student(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
     
+    public String getEmail() {
+        return this.email;
+    }
+
     public void calcResult(){
         double result = this.firstGrade + this.secondGrade + this.thirdGrade;
 
