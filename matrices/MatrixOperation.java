4e0604b0090b1b6ae73f82cb54b572eeac65090a
silvaalcantara <allan.pa.br@gmail.com>
2020-06-07 17:57:42 -0300

    Organizing files

---
+++ matrices/MatrixOperation.java
@@ -0,0 +1,38 @@
+package matrices;
+
+import java.util.Scanner;
+
+public class MatrixOperation {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int [][] mat = new int[n][n];
+
+        for(int i=0; i<mat.length; i++) {
+            for(int j=0; j<mat[i].length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Main diagonal: ");
+        for(int i=0; i<n; i++) {
+            System.out.print(mat[i][i] + " ");
+        }
+
+        int count = 0;
+        for(int i=0; i<mat.length; i++) {
+            for(int j=0; j<mat[i].length; j++) {
+                if(mat[i][j] < 0) count++;
+            }
+        }
+
+        System.out.println("\nNegative numbers = " + count);
+
+        sc.close();
+
+    }
+
+}
